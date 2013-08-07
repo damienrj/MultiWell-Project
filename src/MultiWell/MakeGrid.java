ba77bd61b7be3ced55a67e90bf5de80858d13c5c
Damien <>
2013-08-07 11:46:05 -0700

    Created MakeGrid Class

---
+++ src/MultiWell/MakeGrid.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MultiWell;
+
+import static java.lang.Math.*;
+
+/**
+ *
+ * @author ramunnoj
+ */
+public class MakeGrid {
+
+    public void calculateAngle(double x1, double y1, double x2, double y2) {
+    }
+
+
+}
